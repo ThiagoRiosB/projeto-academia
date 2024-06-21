a494abd77a437079414756249c8865f30798ba0b
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-06-21 19:28:38 -0300

    Iniciei o projeto academia

---
+++ src/app/components/cadastrar-usuario/cadastrar-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastrar-usuario.component.html',
+  styleUrl: './cadastrar-usuario.component.scss'
+})
+export class CadastrarUsuarioComponent {
+
+}
